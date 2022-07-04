7757b135ddd1f4c43921963acd6c4a08d3e27190
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-04 08:38:53 +0300

    axios finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 
 import './App.css';
 import Card from './components/Card';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
+const url = 'https://randomuser.me/api/';
 
 function App() {
+  const [user,setUser] = useState([]);
+
+  const users = async() => {
+  try {
+      const {data} = await axios.get(url);
+      setUser(data.results[0]);
+    
+    
+  } catch (error) {
+    alert(error);
+  }
+}
+
+  useEffect(() => {
+    users();
+
+  }, [])
+
+  
+
   return (
-    <div>
+    <div className='appcont'>
       
-      <Card/>
+      <Card user = {user}/>
+      <button className='btn' onClick={() => users()} >Random User</button>
+
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,50 @@
 import './Card.css';
+import {useState, useEffect} from 'react';
 import emaillogo from '../assets/email.svg';
 import phonelogo from '../assets/phone.svg';
 import adreslogo from '../assets/location.svg';
 
 
-const Card = () => {
+const Card = ({user}) => {
+  const {
+    picture: { medium },
+    name: { title, first, last },
+    email,
+    cell,
+    location: { city, country },
+    dob: { age },
+    registered: { date },
+  } = user;
+
+
+
   return (
     <div className="bigContainer">
 
     <div className='cardContainer'>
-        <div><img src="" alt="" />img</div>
-        <div>name</div>
+        <div><img src={medium} alt="uyuyu" /></div>
+        <div><h3>
+          <span>{title} </span>
+          <span>{first} </span>
+          <span>{last}</span>
+          </h3>
+        </div>
         <div> <img style={{width:'40px'}} src={emaillogo} alt="" /></div>
-        <div>email</div>
+        <div><h3>{email}</h3></div>
         <div> <img style={{width:'40px'}} src={phonelogo} alt="" /></div>
-        <div>phone</div>
+        <div><h3>{cell}</h3></div>
         <div> <img style={{width:'40px'}} src={adreslogo} alt="" /></div>
-        <div>adres</div>
-        <div className='block'>yas</div>
-        <div className='block'>register</div>
+        <div>
+          <h3>
+            <span>{city} / </span>
+            <span>{country}</span>
+          </h3>
+        </div>
+        <div className='block'><h3>Age : {age}</h3></div>
+        <div className='block'><h3>Register Date : {date.substring(0,10)}</h3></div>
     </div>
-    <button className='btn'>Random User</button>
+    
+    
     </div>
   )
 }
